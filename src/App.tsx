063d0dbbfe10756eec25c8ee45296464bccdeb15
AhmedElGarhy1 <gemater.g@gmail.com>
2023-01-30 13:40:46 +0200

    first push

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import NavBar from "./components/NavBar";
+import "./css/App.css";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/data/nav-links.js
@@ -0,0 +1,8 @@
+export default [
+  { id: 1, name: "Home", href: "/" },
+  { id: 2, name: "About", href: "/about" },
+  { id: 3, name: "Courses", href: "/courses" },
+  { id: 4, name: "Articles", href: "/articles" },
+  { id: 5, name: "Postes", href: "/postes" },
+  { id: 6, isLang: true, langs: ["EN", "AR"] },
+];
