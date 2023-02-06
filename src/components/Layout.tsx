79d4f161233e3b24caab24bc15fd81c8e4218aba
AhmedElGarhy1 <gemater.g@gmail.com>
2023-02-06 13:40:54 +0200

    scroll smoothly

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import useLangContext from "../hooks/useLangContext";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 import Loading from "./Loading";
 import useLoadingContext from "../hooks/useLoadingContext";
 
@@ -11,7 +11,14 @@ const Layout = () => {
   const { removeLoading } = useLoadingContext();
   const location = useLocation();
 
-  useEffect(removeLoading, [location]);
+  useEffect(() => {
+    removeLoading();
+    window.scrollTo({
+      behavior: "smooth",
+      left: 0,
+      top: 0,
+    });
+  }, [location]);
 
   return (
     <div dir={isEnglish ? "ltr" : "rtl"}>
