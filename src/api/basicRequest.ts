02a0accedbb9421546586906b6cc113a09d18d80
AhmedElGarhy1 <gemater.g@gmail.com>
2023-04-15 16:42:56 +0200

    fix backend url

--- src/api/basicRequest.ts
+++ src/api/basicRequest.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 
-export const BASE_URL = "http://localhost:7000";
-// export const BASE_URL = "https://tech-mind-backend.onrender.com";
+// export const BASE_URL = "http://localhost:7000";
+export const BASE_URL = "https://tech-mind-backend.onrender.com";
 
 const backendReq = async (url: string, method: string, obj?: object) => {
   const options = {
